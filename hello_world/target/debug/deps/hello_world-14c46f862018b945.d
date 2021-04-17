74f7c4179eb3972d1eca3d3eee851424c45160d8
foo7print <foo7print@users.noreply.github.com>
2021-04-17 09:38:10 +0900

    hello_world

---
+++ hello_world/target/debug/deps/hello_world-14c46f862018b945.d
@@ -0,0 +1,5 @@
+/Users/watarusato/w-sato/workspace/sandbox_of_rust/hello_world/target/debug/deps/hello_world-14c46f862018b945: src/main.rs
+
+/Users/watarusato/w-sato/workspace/sandbox_of_rust/hello_world/target/debug/deps/hello_world-14c46f862018b945.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/watarusato/w-sato/workspace/rust_sandbox/hello_world/target/debug/hello_world: /Users/watarusato/w-sato/workspace/rust_sandbox/hello_world/src/main.rs
